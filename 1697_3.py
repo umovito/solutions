9673b035dbdcd5f1a55127e5ce180ee89dd25af6
AustinYeon <umovito@gmail.com>
2017-08-01 18:45:36 +0900

    1697번

---
+++ 1697_3.py
@@ -0,0 +1,23 @@
+a, b = [int(i) for i in input().split()]
+m=[0] * 100001
+q=[]
+q.append(a)
+c=0
+ing=True
+while ing:
+	qs = len(q)
+	for i in range(qs):
+		m[q[i]] = 1
+		if q[i]!=b:
+			if q[i]-1 >= 0 and m[q[i]-1] == 0:
+				q.append(q[i]-1)
+			if q[i]+1 <= 100000 and m[q[i]+1] == 0:
+				q.append(q[i]+1)
+			if q[i]*2 <= 100000 and m[q[i]*2] == 0:
+				q.append(q[i]*2)
+		else:
+			ing=False
+			print(c)
+			break
+	c+=1
+	q=q[qs:]
